469bb15229a4d86bb1bfc1923a029ad0c93af925
lavanya <lavanyakarra033@gmail.com>
2025-07-29 14:49:45 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-w9fhae_^=o2i%8i9pdtx%j#yf^h+(hqnw&olp!#91dq685r-ls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['budgetbee.onrender.com']
+ALLOWED_HOSTS = ['budgetbee-expensetracker.onrender.com']
 
 
 # Application definition
@@ -116,8 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
